8a83be32ab3bbfa25ca4db4b648dedc6f70c7edb
m3tra <fporto@student.42lisboa.com>
2023-01-26 12:08:12 +0000

    Changed spacing for better readability

--- Module01/ex02/src/main.cpp
+++ Module01/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fporto <fporto@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 05:52:14 by fporto            #+#    #+#             */
-/*   Updated: 2022/08/02 07:23:30 by fporto           ###   ########.fr       */
+/*   Updated: 2023/01/26 12:04:18 by fporto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ int	main() {
 	string*	strPtr = &str;
 	string&	strRef = str;
 
-	std::cout << "str mem address: " << &str << std::endl;
-	std::cout << "strPtr mem address: " << &strPtr << std::endl;
-	std::cout << "strRef mem address: " << &strRef << std::endl;
+	std::cout << "str\t mem address: " << &str << std::endl;
+	std::cout << "strPtr\t mem address: " << &strPtr << std::endl;
+	std::cout << "strRef\t mem address: " << &strRef << std::endl;
 
-	std::cout << "str value: " << str << std::endl;
-	std::cout << "strPtr value: " << strPtr << std::endl;
-	std::cout << "strRef value: " << strRef << std::endl;
+	std::cout << "str\t value: " << str << std::endl;
+	std::cout << "strPtr\t value: " << strPtr << std::endl;
+	std::cout << "strRef\t value: " << strRef << std::endl;
 }
